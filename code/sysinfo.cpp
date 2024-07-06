d1f7e3df1eee158a2caa6e8eb49a587fa6c673a4
Goldside543 <goldside000@outlook.com>
2024-07-06 16:04:11 -0400

    Update sysinfo.cpp

--- code/sysinfo.cpp
+++ code/sysinfo.cpp
@@ -29,3 +29,19 @@ int cpuinfo() {
 
     return 0;
 }
+
+int memoryinfo() {
+    std::ifstream meminfo("/proc/meminfo");
+    std::string line;
+    
+    if (meminfo.is_open()) {
+        while (std::getline(meminfo, line)) {
+            std::cout << line << std::endl;
+        }
+        meminfo.close();
+    } else {
+        std::cout << "Unable to open /proc/meminfo" << std::endl;
+    }
+    
+    return 0;
+}
